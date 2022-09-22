b71eaa8c94b81c1c22350a654c9b70ee13258253
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-09-22 18:01:28 +0900

    change module

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const request = require("request")
-require("dotenv").config()
+const axios = require('axios').default
+require('dotenv').config()
 
 const city = (process.argv[2]) === undefined ? process.env.DEFAULT_CITY : process.argv[2]
 
-const resource = {
-  url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}&lang=ja`,
-  method: "GET",
-  json: true,
+// const rainAmount1h = response.data.rain === undefined ? '-' : response.data.rain['1h']
+
+
+axios
+  .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}&lang=ja`)
+  .then((response) => console.log(weather(response)))
+  .catch(console.error)
+
+function weather (response) {
+  const rainAmount1h = response.data.rain === undefined ? '-' : response.data.rain['1h']
+
+  return `現在の${response.data.name}の天気は${response.data['weather'][0]['description']}\n気温：${response.data.main.temp} ℃\n湿度：${response.data.main.humidity} %\n風速：${response.data.wind.speed} m/sec\n過去1時間の降雨量：${rainAmount1h} mm`;
 }
 
-request(resource, (error, res, body) => {
-  const rainAmount1h = body.rain === undefined ? '-' : body.rain['1h']
-  console.log(`現在の${body.name}の天気は${body['weather'][0]['description']}\n気温：${body.main.temp} ℃\n湿度：${body.main.humidity} %\n風速：${body.wind.speed} m/sec\n過去1時間の降雨量：${rainAmount1h} mm`)
-})
