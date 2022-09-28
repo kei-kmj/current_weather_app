45aa4bfd90e2eccf09093712bcd41f77e8e5893f
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-09-28 14:25:16 +0900

    change method name

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const city = (process.argv[2]) === undefined ? process.env.DEFAULT_CITY : proces
 
 axios
   .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}&lang=ja`)
-  .then((response) => console.log(weather(response)))
+  .then((current_weather) => console.log(format_content(current_weather)))
   .catch(console.error)
 
-function weather (response) {
-  const rainAmount1h = response.data.rain === undefined ? '-' : response.data.rain['1h']
+function format_content (weather) {
+  const rainAmount1h = weather.data.rain === undefined ? '-' : weather.data.rain['1h']
 
-  return `現在の${response.data.name}の天気は${response.data['weather'][0]['description']}\n気温：${response.data.main.temp} ℃\n湿度：${response.data.main.humidity} %\n風速：${response.data.wind.speed} m/sec\n過去1時間の降雨量：${rainAmount1h} mm`
+  return `現在の${weather.data.name}の天気は${weather.data['weather'][0]['description']}\n気温：${weather.data.main.temp} ℃\n湿度：${weather.data.main.humidity} %\n風速：${weather.data.wind.speed} m/sec\n過去1時間の降雨量：${rainAmount1h} mm`
 }
 
